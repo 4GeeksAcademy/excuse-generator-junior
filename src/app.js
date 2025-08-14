8c48d7e0a0845c405e50cd5aa80add035397a8d0
Junior Sequera <35280577+junseiken@users.noreply.github.com>
2025-08-14 01:42:00 +0000

    agregar actualizacion js y html

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  let elementExcuse = document.getElementById("excuse");
+
+  let who = ["Mi perro", "Mi gato", "Mi hermana", "Una paloma"];
+  let action = ["comio", "daño", "escondio", "choco"];
+  let what = ["mis notas", "mi telefono ", "la moto "];
+  let when = ["despues del trabajo","cuando estaba comiendo","cuando sali","mientras estaba en el baño","durante la practica",
+  ];
+
+  function setTexts() {
+    elemenExcuse.innerText = aggExcuse(who, action, what, when) + "...";
+  }
+ 
+  setTexts();
 };
+function randomText(array){
+  let text = Math.floor(Math.random() * array.length);
+  return text;
+}
+
+
+function aggExcuse(who, action, what, when) {
+  let excuseAggArray []
+}
